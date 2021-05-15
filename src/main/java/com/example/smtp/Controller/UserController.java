2c79ca9cb478755522e1ffff350739b07a3690c6
葬我于高山之上兮 <55906505+jbc2212321@users.noreply.github.com>
2021-05-15 12:50:31 +0800

    5/15 修改返回值类型

--- src/main/java/com/example/smtp/Controller/UserController.java
+++ src/main/java/com/example/smtp/Controller/UserController.java
@@ -16,12 +16,15 @@ public class UserController {
     //用户登录
     @CrossOrigin
     @RequestMapping(value = "/checkUser", method = RequestMethod.POST, consumes = "application/json")
-    public Boolean checkUser(@RequestBody String jsonParamStr) {
+    public String checkUser(@RequestBody String jsonParamStr) {
         JSONObject jsonObject = JSONObject.parseObject(jsonParamStr);
         String userName = jsonObject.getString("username");
         String password = jsonObject.getString("password");
         int check = userMapper.checkUser(userName, password);
-        return check == 1;
+        if (check==1){
+            return "登录成功";
+        }
+        return "登录失败";
 
     }
 
@@ -41,14 +44,14 @@ public class UserController {
     //添加用户
     @CrossOrigin
     @RequestMapping(value = "/addUser", method = RequestMethod.POST, consumes = "application/json")
-    public Boolean addUser(@RequestBody String jsonParamStr) {
+    public String addUser(@RequestBody String jsonParamStr) {
         boolean isRegister = false;
         JSONObject jsonObject = JSONObject.parseObject(jsonParamStr);
         String userName = jsonObject.getString("username");
         String passWord = jsonObject.getString("password");
         String email = userName + "@dyz.com";
         userMapper.addUser(userName, passWord, email);
-        return true;
+        return "注册成功";
     }
 
     //查看用户列表
